35ed22d138d733f0c1c6365001b464ec165d9b92
Andrew Carpenter <andrew@audioinhd.com>
2013-03-06 01:22:19 -0500

    Create gitignored-cleanup.sh

---
+++ gitignored-cleanup.sh
@@ -0,0 +1,29 @@
+giti=.gitignore
+
+if [ -f $giti ]
+
+then
+
+  while read line
+  do
+    [[ $line = \#* ]] && continue
+    [[ $line = \=* ]] && continue
+    [[ -z "$line" ]] && continue
+    
+    # Toggle comment on all four git commands to test before executing
+
+  case $line in
+	/*) 
+		declare gitrmd=$(echo $PWD$line)
+		git rm -r --cached --ignore-unmatch -- $gitrmd 2>/dev/null
+		#git rm -n -r --cached --ignore-unmatch -- $gitrmd
+	;&
+	*)	
+		git rm -r --cached --ignore-unmatch -- $line 2>/dev/null
+		#git rm -n -r --cached --ignore-unmatch -- $line
+	;;
+	esac
+	
+  done < $giti
+
+fi
